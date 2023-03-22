c9bbc72dd9d384ea14f2ddb879652e5c764b4388
jangjo123 <cjkh1324@naver.com>
2023-03-22 12:52:09 +0900

    Sets

--- main.dart
+++ main.dart
@@ -1,23 +1,4 @@
 void main() {
-  var player = {
-    'name': 'gunal',
-    'xp': 19.99,
-    'superpower': false,
-  };
-
-  Map<int, bool> player2 = {
-    1: true,
-    2: false,
-    3: true,
-  };
-
-  Map<List<int>, bool> test = {
-    [1, 2, 3, 4]: true,
-  };
-
-  List<Map<String, Object>> players = [
-    {'name': "gunal", 'xp': 19953.36},
-    {'name': "gunal", 'xp': 19953.36},
-    {'name': "gunal", 'xp': 19953.36},
-  ];
+  var numbers = {1, 2, 3, 4};
+  Set<int> numbers2 = {1, 2, 3, 4};
 }
