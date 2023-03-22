f165a458f940ae600f9b0db862d15ceaee0839fa
jangjo123 <cjkh1324@naver.com>
2023-03-22 12:36:56 +0900

    Collection for

--- main.dart
+++ main.dart
@@ -1,6 +1,10 @@
 void main() {
-  var name = 'gunal';
-  var age = 10;
-  var greeting = "Hello everyone, my name is $name and I'm ${age + 10}";
-  print(greeting);
+  var oldFriends = ['gunal', 'subin'];
+  var newFriends = [
+    'lewis',
+    'ralph',
+    'darren',
+    for (var friend in oldFriends) "❤ $friend",
+  ];
+  print(newFriends);
 }
