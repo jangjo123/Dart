d225e4245fe28ed510a5c09960af8b6dd88074e5
jangjo123 <cjkh1324@naver.com>
2023-03-22 11:02:01 +0900

    Recap

--- main.dart
+++ main.dart
@@ -1,5 +1,29 @@
 void main() {
-  // const는 compile-time에 이미 알고 있는 값
-  // 아니면 final or var
-  const API = ;
+  // 복습
+  int i = 12; // class에 property 작성에 사용
+  var name = 'gunal'; // 지역변수에 사용
+  i = 121231512;
+  name = 'asdad';
+
+  final name2 = 'gunal';
+  name2 = '1222'; // 에러
+
+  dynamic name3; // 어떤 타입인지 모름
+  name3 = '122';
+  name3 = 12;
+  name3 = true;
+
+  if (name3 is String) {
+    // 어떤 타입인지 모르니 체크
+  }
+
+  const api_key = '1321313';
+  api_key = '343'; // 에러
+
+  String? name4 = 'gunal';
+  name4 = null;
+  name4?.isEmpty;
+
+  late final String name5; // 데이터는 나중에 넣겠다.
+  name5 = '12';
 }
