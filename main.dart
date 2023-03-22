415e8ca16e43a64ff564926aca98eed9932269fb
jangjo123 <cjkh1324@naver.com>
2023-03-22 12:24:12 +0900

    Lists

--- main.dart
+++ main.dart
@@ -1,12 +1,15 @@
 import 'dart:ffi';
 
 void main() {
-  String name = "gunal";
-  bool alive = true;
-  int age = 20;
-  double money = 69.99;
-
-  // num은 int, double의 부모 class
-  num x = 12;
-  x = 1.1;
+  var giveMeFive = true;
+  List<int> numbers = [
+    1,
+    2,
+    3,
+    4,
+    if (giveMeFive) 5,
+  ];
+  numbers.add(1);
+  print(numbers.last);
+  print(numbers);
 }
