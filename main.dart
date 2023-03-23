64a5e5c76db95c9bc1bd0d6fcf6d63cd17f64716
jangjo123 <cjkh1324@naver.com>
2023-03-23 14:56:13 +0900

    Optional Positional Parameters

--- main.dart
+++ main.dart
@@ -1,14 +1,7 @@
-String sayHello({
-  required String name,
-  int age = 10,
-  String country = 'Korea',
-}) =>
-    "Hello $name, you are $age, and you come from $country";
+String sayHello(String name, int age, [String? country = 'Korea']) =>
+    'Hello $name, you are $age years old from $country';
 
 void main() {
-  sayHello(
-    name: "gunal",
-    age: 20,
-    country: "korea",
-  );
+  var results = sayHello('gunal', 12);
+  print(results);
 }
