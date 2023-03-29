211756654faf8503790d00aba9e4a3cc6556fa27
jangjo123 <cjkh1324@naver.com>
2023-03-29 11:17:44 +0900

    Abstract classes

--- main.dart
+++ main.dart
@@ -1,8 +1,12 @@
+abstract class Human {
+  void walk();
+}
+
 enum Team { red, blue }
 
 enum XPLevel { beginner, medium, pro }
 
-class Player {
+class Player extends Human {
   late String name;
   late XPLevel xp;
   late Team team;
@@ -13,8 +17,18 @@ class Player {
     required team,
   });
 
+  void walk() {
+    print("im walking");
+  }
+
   void sayHello() {
     print("Hi my name is ${name}, $xp, $team");
+  }
+}
+
+class Coach extends Human {
+  void walk() {
+    print("the coach is walking");
   }
 }
 
