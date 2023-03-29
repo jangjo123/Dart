bf1986000a30726637a87f9754474b414351c3fa
jangjo123 <cjkh1324@naver.com>
2023-03-29 11:37:55 +0900

    Inheritance

--- main.dart
+++ main.dart
@@ -1,41 +1,33 @@
-abstract class Human {
-  void walk();
+class Human {
+  final String name;
+  Human(this.name);
+  void sayHello() {
+    print("Hi my name is $name");
+  }
 }
 
 enum Team { red, blue }
 
-enum XPLevel { beginner, medium, pro }
-
 class Player extends Human {
-  late String name;
-  late XPLevel xp;
-  late Team team;
+  final Team team;
 
   Player({
-    required name,
-    required xp,
-    required team,
-  });
+    required this.team,
+    required String name,
+  }) : super(name);
 
-  void walk() {
-    print("im walking");
-  }
-
+  @override
   void sayHello() {
-    print("Hi my name is ${name}, $xp, $team");
-  }
-}
-
-class Coach extends Human {
-  void walk() {
-    print("the coach is walking");
+    super.sayHello();
+    print("and I player for ${team}");
   }
 }
 
 void main() {
-  var gunal = Player(name: 'gunal', xp: 1200, team: Team.blue)
-    ..name = 'la' // gunal.name = 'la'
-    ..xp = XPLevel.medium
-    ..team = Team.red
-    ..sayHello();
+  var player = Player(
+    team: Team.red,
+    name: "gunal",
+  );
+
+  player.sayHello();
 }
