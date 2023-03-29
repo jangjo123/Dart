0733b7c9d5f690f56f95b834152d2dd031f503f0
jangjo123 <cjkh1324@naver.com>
2023-03-29 10:17:49 +0900

    First Dart Class

--- main.dart
+++ main.dart
@@ -1,17 +1,17 @@
-typedef ListOfInts = List<int>;
+class Player {
+  final String name = 'gunal';
+  int xp = 1500;
 
-ListOfInts reverseListOfNumbers(ListOfInts list) {
-  var reversed = list.reversed;
-  return reversed.toList();
-}
-
-typedef UserInfo = Map<String, String>;
-
-String SayHi(UserInfo userInfo) {
-  return "Hi ${userInfo['name']}";
+  void sayHello() {
+    var name = 123;
+    print("Hi my name is ${this.name}"); // 이런 경우가 아니라면 this는 안쓰는게 좋음
+  }
 }
 
 void main() {
-  print(reverseListOfNumbers([1, 2, 3]));
-  print(SayHi({"name": "gunal"}));
+  var player = Player();
+  print(player.name);
+  player.sayHello();
+  // player.name = 'lalala'; // final때문에 실행 불가
+  // print(player.name);
 }
